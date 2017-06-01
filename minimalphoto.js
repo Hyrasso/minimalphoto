e4f1e5f134b17048883f12e6fffebfff8fbb87c0
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2017-06-01 19:11:45 +0200

    remove old code didnt work

--- minimalphoto.js
+++ minimalphoto.js
@@ -50,58 +50,6 @@ function* iterdraw(im) {
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  hdens = round(im.height / hdens);
-  wdens = round(im.width / wdens);
-  dim = createImage(wdens, im.height/hdens);
-  dim.copy(im, 0, 0, im.width/wdens, im.height, 0, 0, dim.width, dim.height);
-
-  hdens = im.height / hdens;
-  wdens = im.width / wdens;
-
-  
-  // deformation way
-  //image(im, 0, 0);
-  image(dim, 0, im.height);
-  
-  var shuf = [];
-  for(var i = 0; i < (dim.width + 1) * (dim.height + 1); i++) {
-    if (i > dim.width * dim.height || i < dim.width || i % dim.width === 0) {
-      shuf[i] = [0, 0];
-    } else {
-        shuf[i] = [Math.trunc(random(-wdens/2, wdens/2)),
-                   Math.trunc(random(-hdens/2, hdens/2))];
-    }
-  }
-  console.log(shuf);
-  var idx, col, r, g, b, dx, dy;
-  dim.loadPixels();
-  for(var x = 0;x < dim.width;x++) {
-    for(var y = 0;y < dim.height;y++) {
-      dx = shuf[x + y * dim.width][0];
-      dy = shuf[x + y * dim.width][1];
-      var dx10 = shuf[x + 1 + y * dim.width][0];
-      var dy10 = shuf[x + 1 + y * dim.width][1];
-      var dx01 = shuf[x + (y + 1) * dim.width][0];
-      var dy01 = shuf[x + (y + 1) * dim.width][1];
-      var dx11 = shuf[x + 1 + (y + 1) * dim.width][0];
-      var dy11 = shuf[x + 1 + (y + 1) * dim.width][1];
-      
-      idx = 4 * (x + y * dim.width);
-      r = dim.pixels[idx];
-      g = dim.pixels[idx + 1];
-      b = dim.pixels[idx + 2];
-      fill(r, g, b);
-      stroke(r, g, b);
-      beginShape();
-      vertex(x * wdens * dim.width + dx, y * hdens * dim.height + dy);
-      vertex((x + 1) * wdens * dim.width + dx10, y * hdens * dim.height + dy10);
-      vertex((x + 1) * wdens * dim.width + dx11, (y + 1) * hdens * dim.height + dy11);
-      vertex(x * wdens * dim.width + dx01, (y + 1) * hdens * dim.height + dy01);
-      endShape(CLOSE);
-    }
-  }
-  dim.updatePixels();
-  
   
   iterator = iterdraw(im);
 
